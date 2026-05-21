f301bd32652934ec531a8532be472241cd4de321
himani <himanisabre@gmail.com>
2026-05-21 18:07:22 +0530

    Final list comprehension:

--- practise.py
+++ practise.py
@@ -298,38 +298,141 @@
 # print(list_2)
 # print(list_1)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[1:3]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[1:3]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[1:-1]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[1:-1]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[-1:1]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[-1:1]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[-5:3]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[-5:3]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[:3]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[:3]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-new_list = my_list[2:]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[2:]
+# print(new_list)
 
-my_list = [10,8,6,4,2]
-del my_list[1:3]
-print(my_list)
-del my_list[:]
-print(my_list)
+# my_list = [10,8,6,4,2]
+# del my_list[1:3]
+# print(my_list)
+# del my_list[:]
+# print(my_list)
 
-my_list = [0,3,12,8,2]
-print(5 in my_list)
-print(5 not in my_list)
-print(12 in my_list)
+# my_list = [0,3,12,8,2]
+# print(5 in my_list)
+# print(5 not in my_list)
+# print(12 in my_list)
 
+# row = []
+# for i in range(8):
+#     row.append("WHITE_PAWN")
+# print(row)
+
+# row = ["WHITE_PAWN" for i in range(8)]
+# print(row)
+
+# squares = [x**2 for x in range(10)]
+# print(squares)
+
+# squares = [x**2 for x in range(1,11)]
+# print(squares)
+
+# squares = [index**2 for index in range(1,11)]
+# print(squaresb)
+
+# twos = [2** index for index in range(8)]
+# print(twos)
+
+# squares = [index**2 for index in range(1,11)]
+# odds = [element for element in squares if element % 2 != 0]
+# print(odds)
+
+# board = []
+# for i in range(8):
+#     row =["EMPTY" for i in range(8)]
+#     board.append(row)
+# # print(board)
+# for index in board:
+#  print(index)
+ 
+# print(len(board))
+
+# print(board[0][0])
+# print("-----------")
+# board[0][0] = "Rooks"
+# board[0][7] = "Rooks"
+# board[7][0] = "Rooks"
+# board[7][7] = "Rooks"
+# for element in board:
+#    print(element)
+
+# board[0][1] = "knight"
+# board[0][6] = "knight"
+# board[7][1] = "knight"
+# board[7][6] = "knight"
+ 
+# print("----------------")
+# for element in board:
+#    print(element)
+
+# temps = [[0.0 for h in range(24)]for d in range(31)]
+# temp1 = 19
+# temp2 = 32
+# count = 0
+# for days in temps:
+#     if count == 0:
+#         days[11] = temp1
+#         count = 1
+#     else:
+#         days[11] = temp2   
+#         count = 0
+# for element in temps:
+#     print(element)
+  
+# total = 0.0
+# for day in temps:
+#    total += day[11]
+
+# average = total/31
+# print("average temperature at noon:",average)
+
+# highest = -100.0
+# for day in temps:
+#     for temp in day:
+#         if temp > highest:
+#             highest = temp
+# print("the highest temperature was:",highest)
+
+# hot_days = 0
+# for day in temps:
+#     if day[11] > 20.0:
+#         hot_days += 1
+# print(hot_days,"days were hot days in the month.")
+
+rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+print(rooms)
+
+rooms[1][9][13] = True
+
+rooms[1][9][1] = True
+
+vacancy = 0
+for room_number in range(20):
+    if not rooms[1][9][room_number]:
+        vacancy += 1
+print("vacancy in 3rd 15th floor of 3rd building",vacancy)
+
+
+ 
+  
+  
