b4996c3a4bd578b6b592bb3ce4f957ccba49c348
himani <himanisabre@gmail.com>
2026-05-28 17:34:11 +0530

    tuple & dictionary

--- practise.py
+++ practise.py
@@ -419,20 +419,298 @@
 #         hot_days += 1
 # print(hot_days,"days were hot days in the month.")
 
-rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
-print(rooms)
+# rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+# print(rooms)
 
-rooms[1][9][13] = True
+# rooms[1][9][13] = True
 
-rooms[1][9][1] = True
+# rooms[1][9][1] = True
 
-vacancy = 0
-for room_number in range(20):
-    if not rooms[1][9][room_number]:
-        vacancy += 1
-print("vacancy in 3rd 15th floor of 3rd building",vacancy)
+# vacancy = 0
+# for room_number in range(20):
+#     if not rooms[1][9][room_number]:
+#         vacancy += 1
+# print("vacancy in 3rd 15th floor of 3rd building",vacancy)
 
+# def scope_test():
+#     x = 123
+# scope_test()
+# print(x)
 
+# def my_function():
+#     print("Do I  know the variable?",var)
+
+# var = 1
+# my_function()
+# print(var)
+
+# def mult(x):
+#     var = 7
+#     return x*var
+# var = 3
+# print(mult(7))
+
+# def my_function():
+#     global var
+#     var = 2
+#     print("Do I know that variable?",var)
+
+# var = 1
+# my_function()
+# print(var)
  
+# var = 2
+# print(var)
+
+# def return_var():
+#     global var
+#     var = 5
+#     return var
+# print(return_var())
+# print(var)
+
+# def my_function(n):
+#     print("I got",n)
+#     n += 1
+#     print("I have",n)
+
+# var = 1
+# my_function(var)
+# print(var)
+
+# def my_function(my_list_1):
+#     print("print #1:",my_list_1)
+#     print("print #2:",my_list_2)
+#     my_list_1 = [0,1]
+#     print("print #3:",my_list_1)
+#     print("print #4:",my_list_2)
+
+# my_list_2 = [2,3]
+# my_function(my_list_2)
+# print("print#5:",my_list_2)
+
+# def my_function(my_list_1):
+#     print("print #1:",my_list_1)
+#     print("print #2:",my_list_2)
+#     del my_list_1[0]
+#     print("print #3:",my_list_1)
+#     print("print #4:",my_list_2)
+
+# my_list_2 = [2,3]
+# my_function(my_list_2)
+# print("print#5:",my_list_2)
+
+# def countdown(number):
+#     print(number)
+#     if number == 0:
+#         return
+#     else:
+#         print("going in rec:",number)
+#         countdown(number-1)
+#         print("out of rec:",number)
+# print("starting recursion")        
+# countdown(5) 
+# print("completed recursion") 
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# print("Factorial of 5 is:", factorial(5)) 
+
+# my_tuple = (1,10,100)
+# t1 = my_tuple + (1000,10000)
+# t2 = my_tuple*3
+# print(len(t2))
+# print(t1)
+# print(t2)
+# print(10 in my_tuple)
+# print(-10 not in my_tuple)
+
+# my_tuple = (10,100,1000)
+# my_tuple += (10000,100000)
+# print(my_tuple)
+
+# tuple_1 = (1,2,3)
+# for elem in tuple_1:
+#     print(elem)
+
+# tuple_2 = (1,2,3,4)
+# print(5 in tuple_2)
+# print(5 not in tuple_2)
+
+# tuple_3 = (1,2,3,4)
+# print(len(tuple_3))
+# print(5 not in tuple_3)
+
+# tuple_4 = tuple_1 + tuple_2
+# tuple_5 = tuple_3*2
+# print(tuple_4)
+# print(tuple_5)
+
+# my_tuple = tuple((1,2,"string"))
+# print(my_tuple)
+# print(type(my_tuple))
+
+# my_list = [2,4,6]
+# print(my_list)
+# print(type(my_list))
+# tup = tuple(my_list)
+# print(tup)
+# print(type)
+
+# var = 123
+# t1 = (1,)
+# t2 = (2,)
+# t3 = (3, var)
+# t1, t2, t3 = t2, t3, t1
+# print(t1,t2,t3)
+# print(type(t1),type(t2),type(t3))
+
+# dictionary = {"cat":"chat","dog":"chien","horse":"cheval"}
+# phone_number = {'boss':123456789,'suzy':987654321}
+# empty_dictionary = {}
+# print(dictionary)
+# print(type(dictionary))
+# print((phone_number))
+# print(type(phone_number))
+# print(empty_dictionary)
+# print(type(empty_dictionary))
+# print(dictionary['cat'])
+# print(phone_number['suzy'])
+# # print(phone_number['president'])
+
+# words = ['cat','lion','horse']
+# for word in words:
+#     if word in dictionary:
+#         print(word,"->",dictionary[word])
+#     else:
+#         print("----",word,"is not in dictionary","----")
+
+#     print(dictionary.keys())
+#     for key in dictionary.keys():
+#          print(key,"->",dictionary[key])
+
+#     for key, value in dictionary.items():
+#         print(key,"->",value)
+#         print(dictionary.value())
+#         for value in dictionary.values():
+#             print(value)
+
+# pol_eng_dictionary = {
+#     "zamek":"castle",
+#     "woda":"water",
+#     "gleba":"soil"
+#     }
+# print ("poly_eng_dictionary:",pol_eng_dictionary)
+# copy_dictionary = pol_eng_dictionary.copy()
+# print("copy_dictionary:",copy_dictionary)
+# pol_eng_dictionary["zamek"] = "lock"
+# item = pol_eng_dictionary["zamek"]
+# print(item)
+
+# phonebook = {}
+# print(phonebook)
+# phonebook["Adam"] = 98765432
+# print(phonebook)
+# del phonebook["Adam"]
+# print(phonebook)
+
+# pol_eng_dictionary = {"kwiat":"flower"}
+# pol_eng_dictionary.update(
+#     {
+#         "gleba":"soil"
+#     })
+# print(pol_eng_dictionary)
+# pol_eng_dictionary.popitem()
+# print(pol_eng_dictionary)
+
+# pol_eng_dictionary = {
+#     "zamek":"castle",
+#     "woda":"water",
+#     "gleba":"soil"
+# }
+# if "zamek1" in pol_eng_dictionary:
+#     print("yes! zamek1 is presnet in dictionary")
+# else:
+#     print("no! zamek1 is not present in the dictionary")
+
+# print(pol_eng_dictionary)
+# print(len(pol_eng_dictionary))
+
+# del pol_eng_dictionary["zamek"]
+# print(pol_eng_dictionary)
+# print(len(pol_eng_dictionary))
+
+# pol_eng_dictionary.clear()
+# print(pol_eng_dictionary)
+# print(len(pol_eng_dictionary))
+
+# del pol_eng_dictionary
+# print(pol_eng_dictionary)
+
+sd = {}
+while True:
+    name = input("enter student's name:")
+    if name == "":
+        break
+    score = int(input(f"enter {name}'s score:"))
+    if score not in range(1,11):
+        break
+    if name in sd:
+        sd[name] += (score, )
+    else:
+        sd[name] = (score, )
+print(sd)
+for name, mark in sd.items():
+    sum = 0
+    for m in mark:
+        sum += m
+        print(name,"->",sum/len(mark))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
--- practise2.py
+++ practise2.py
@@ -1,7 +1,3 @@
 # print("testing comit changes")
-list = [8,10,6,2,4]
-for i in range(len(list)):
-    for j in range(len(list)-1):
-     if list[j] > list[j+1]:
-       list[j], list[j+1] = list[j+1], list[j]
+
        
